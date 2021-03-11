957e95320c12a8680dbacfe29c8b8c4092de8e1d
MightyK <KMightyDev@gmail.com>
2021-03-11 13:05:05 -0500

    Click functionality implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [headingText, setHeadingText] = useState('Hello')
+
+    const handleClick = () => {
+        setHeadingText(`Hello ${firstName} ${lastName}`)
+    }
+
     return (
         <div className='container'>
             <h1>Hello</h1>
             <form>
                 <input name='fName' placeholder='First Name' />
                 <input name='lName' placeholder='Last Name' />
-                <button>Submit</button>
+                <button 
+                    onClick={handleClick}
+                >
+                    Submit
+                </button>
             </form>
         </div>
     );  
